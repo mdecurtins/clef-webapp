a263a6ff28106de8ea28483ca74c2e9638d05baf
Max DeCurtins <mdecurti@hsph.harvard.edu>
2018-03-24 17:02:17 -0400

    refactoring, adding Redux dispatching

--- src/components/FlatEmbed/FlatEmbed.js
+++ src/components/FlatEmbed/FlatEmbed.js
@@ -1,37 +1,73 @@
 import React from 'react';
 import Embed from 'flat-embed';
 import './FlatEmbedStyles.css';
+import { connect } from 'react-redux';
+import { flatEmbed } from "../../actions/ClefActions";
 
-export default class FlatEmbed extends React.Component {
+
+class FlatEmbed extends React.Component {
+
 
     componentDidMount() {
         // Create the Flat editor embed after the container has been rendered to the DOM.
+        this.embed = this.getEmbed();
         this.editor();
+
+        // Dispatch an action to store the Flat Embed object instance in the application state.
+        const { flatEmbed } = this.props;
+        flatEmbed( this.embed );
     }
 
+
+    constructor( props ) {
+        super( props );
+    }
 
     /**
      * Creates a container for a Flat.io embedded editor
      */
     editor() {
+
+        this.embed.loadFlatScore( '5a9c6cea1a24f30e79374258' ).then( function () {
+            // Success.
+        }).catch( function ( err ) {
+            // Failure.
+            console.log( err );
+        });
+    }
+
+    getEmbed() {
         const flatContainer = document.getElementById( 'flat-container' );
 
-        let embed = new Embed( flatContainer, {
+        if ( typeof this.embed !== 'undefined' ) {
+            return this.embed;
+        }
+
+        return new Embed( flatContainer, {
             embedParams: {
                 appId: '5a3ac8f2f88679130280d7ab',
                 mode: 'edit'
             }
         });
-
-        embed.loadFlatScore( '5a9c6cea1a24f30e79374258' ).then( function () {
-
-        }).catch( function ( err ) {
-            // Did we fail to load the
-            console.log( err );
-        });
     }
 
     render() {
-
+        return (
+            <div id="flat-container"></div>
+        );
     }
 }
+
+function mapStateToProps( state ) {
+    return {
+        view: state.view
+    };
+}
+
+function mapDispatchToProps( dispatch ) {
+    return {
+        flatEmbed
+    };
+}
+
+export default connect( mapStateToProps, mapDispatchToProps )( FlatEmbed );
