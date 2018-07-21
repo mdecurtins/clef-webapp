33bd322cafa92d773f616749dee451f547ca3b4b
Max DeCurtins <mdecurti@hsph.harvard.edu>
2018-07-21 13:43:33 -0400

    datasets container, first pass

---
+++ src/components/Datasets/DatasetsContainer.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {getDatasets} from "../../actions/ClefActions";
+import Dataset from './Dataset';
+
+/**
+ * Class to handle display of datasets available for searching.
+ *
+ * @since 1.0.0
+ */
+class DatasetsContainer extends React.Component {
+
+    constructor( props ) {
+        super( props );
+
+        this.displayDatasets.bind( this );
+    }
+
+
+    /**
+     * Fires after the initial render of this component in the DOM.
+     *
+     * @since 1.0.0
+     */
+    componentDidMount() {
+        this.props.fetchDatasets();
+    }
+
+
+    /**
+     * Gets the HTML to display datasets available for searching.
+     *
+     * @since 1.0.0
+     * @return {Array}
+     */
+    displayDatasets() {
+        let dsets = [];
+        if ( Array.isArray( this.props.datasets ) && this.props.datasets.length > 0 ) {
+            this.props.datasets.map( function ( dataset, i ) {
+                dsets.push( <Dataset key={i} dset={dataset} position={i} /> );
+            });
+        }
+        return dsets;
+    }
+
+
+    /**
+     * Renders this component to the DOM.
+     *
+     * @since 1.0.0
+     * @return {*}
+     */
+    render() {
+        return (
+            <div id="clef-datasets">
+                <h3>Datasets</h3>
+
+            </div>
+        );
+    }
+}
+
+function mapStateToProps( state ) {
+    return {
+        datasets: state.datasets
+    };
+}
+
+function mapDispatchToProps( dispatch ) {
+    return {
+        fetchDatasets: function () {
+            dispatch( getDatasets() );
+        }
+    };
+}
+
+export default connect( mapStateToProps, mapDispatchToProps )( DatasetsContainer );
