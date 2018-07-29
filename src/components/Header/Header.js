02e656d8c2a2075cfbb630594dd368409acd0476
Max DeCurtins <mdecurti@hsph.harvard.edu>
2018-07-29 13:02:36 -0400

    header instructions and styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Header.css';
 
 export default class Header extends React.Component {
     render() {
         return (
-            <div></div>
+            <div className="clef-header">
+                <h2>Welcome to the Clef web application!</h2>
+                <p>Instructions:</p>
+                <ol>
+                    <li>Select one or more algorithms to run.</li>
+                    <li>Select one or more datasets to search.</li>
+                    <li>Enter some music in the editor below, and click "Search".</li>
+                    <li>See your results!</li>
+                </ol>
+            </div>
         );
     }
 }
