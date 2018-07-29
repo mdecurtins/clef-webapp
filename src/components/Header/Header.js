3adba6a9c9629ff943fb464dbcc22a3a66f05064
Max DeCurtins <mdecurti@hsph.harvard.edu>
2018-07-29 11:32:41 -0400

    removed dummy text

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Header extends React.Component {
     render() {
         return (
-            <h1 className="header">{this.props.name}</h1>
+            <div></div>
         );
     }
 }
